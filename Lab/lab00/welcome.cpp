194ca510a8a573db846fd9e2e5632b1e0fc0ed37
Qing <42001432+QingC0913@users.noreply.github.com>
2022-03-19 16:09:17 -0400

    Add files via upload

--- Lab/lab00/welcome.cpp
+++ Lab/lab00/welcome.cpp
@@ -1,1 +1,30 @@
+// welcome.cpp
+//
+// Purpose: Welcome the squad to their new favorite class.
+// Author:  Megan Monroe (mmonroe)
+// Edited by Qing Cheng, 2/2/2021 
+//
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main()
+{
+        string name;
+        
+        // Get the user's name
+        cout << endl;
+        cout << "Hello!";
+        cout << endl;
+        cout << "Please enter your name: ";
+        cin >> name;
+
+        // Print the welcome message
+        cout << endl;
+        cout << name << " is excited to start the CS11 class! Yay! ";
+        cout << "Welcome to Comp 11!" << endl;
+        cout << endl;
+
+        return 0;
+} 
